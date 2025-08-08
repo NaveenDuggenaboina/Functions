038c344d0334b10746817409c8b0059ca11c004a
Duggenaboina Naveen <119068643+NaveenDuggenaboina@users.noreply.github.com>
2025-08-08 12:49:53 +0530

    Add files via upload

---
+++ FilterMapReduceEx.py.py
@@ -0,0 +1,6 @@
+#FilterMapReduceEx.py
+print("enter list of Salariesn separated by space:")
+sals=[float(sal) for sal in input().split() if 0<=float(sal)<=1000]
+#obtain salaries ranges from o to 501
+sal0_500=list(filter(lambda sal:))
+print(sals)
